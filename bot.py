aeb9700ec56d11abe8e1ce6f598bcd8f89c4bb1e
kocqous1992-rgb <kocqous1992@gmail.com>
2026-06-28 09:16:05 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,17 +8,12 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bonjour ! Mon bot fonctionne 👍")
 
 def main():
-    if not TOKEN:
-        print("TOKEN manquant")
-        return
-
     app = Application.builder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
 
     print("Bot started")
 
-    # IMPORTANT : PAS DE asyncio.run
     app.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
